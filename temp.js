0a2eeb5b76f68ceda6c5c1dd933e2af53db23e27
zsjsll <124119885@qq.com>
2024-01-05 23:20:59 +0800

    4

--- temp.js
+++ temp.js
@@ -5,21 +5,23 @@
  * @return {*}
  */
 function curry(fn, args) {
-    let slice = Array.prototype.slice
-    args = slice.call(arguments, 1)
-    console.log("[ args ]-10", args)
+    function toArray(a) {
+        let arr = []
+        for (let iterator of a) {
+            arr.push(iterator)
+        }
+        return arr
+    }
+    let args_list = toArray(arguments)
+    console.log(args_list)
+
+    args = args_list.slice(1)
 
     if (fn.length <= args.length) return fn.apply(null, args)
     else {
         return function (_args) {
-            console.log("[ _args ]-16", arguments)
-            _args = slice.call(arguments, 0)
-            console.log("[ _args ]-17", _args)
-            args = args.concat(_args)
-            args.unshift(fn)
-            console.log(args)
-
-            return curry.apply(null, args)
+            _args = args_list.concat(toArray(arguments))
+            return curry.apply(null, _args)
         }
     }
 }
@@ -33,4 +35,4 @@ function t(a) {
 }
 
 let b = curry(add, 1)
-console.log(b(2) (3))
+console.log(b(2, 3))
