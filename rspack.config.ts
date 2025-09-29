60a53051f784afca8e14fecb24ff3113b7c70123
zsjsll <124119885@qq.com>
2025-09-29 16:30:53 +0800

    11

--- rspack.config.ts
+++ rspack.config.ts
@@ -1,10 +1,61 @@
 // import AutoxDeployPlugin from "./webpack/autox-deploy-webpack-plugin/index.js"
 // import { SwcJsMinimizerRspackPlugin } from "@rspack/core"
-import type { RspackOptions } from "@rspack/core"
+import type { RspackOptions, Compiler, RspackPluginInstance } from "@rspack/core"
 import path from "path/posix"
+import { get } from "http"
 
-// const entry_path = "./src"
-// const entry_file = path.resolve(path.join(entry_path, "main.ts"))
+// 自己编写的插件
+
+class AutoxDeployPlugin implements RspackPluginInstance {
+  opt = { type: undefined, path: undefined }
+
+  constructor(options = this.opt) {
+    this.options = options
+  }
+
+  sendCmd(cmd, path) {
+    console.error("执行命令：", cmd)
+    path = encodeURI(path)
+    const req = get("http://127.0.0.1:9317/exec?cmd=" + cmd + "&path=" + path, (res) => {
+      res.setEncoding("utf8")
+      res
+        .on("data", (data) => {
+          console.error(data)
+        })
+        .on("error", () => {
+          console.error("返回数据错误")
+        })
+    })
+    req.on("error", () => {
+      console.error("watch模式，自动" + cmd + "失败,autox.js服务未启动")
+      console.error("请使用 ctrl+shift+p 快捷键，启动auto.js服务")
+    })
+  }
+
+  apply(compiler: Compiler) {
+    if (this.options === this.opt) return console.log("没有options，不进行任何操作！")
+    compiler.hooks.done.tap("AutoxDeployPlugin", () => {
+      if (typeof this.options.path !== "string") {
+        throw new Error("必须提供一个有效的相对路径")
+      }
+      const out = posix.resolve(this.options.path)
+      console.log("----->[out_file_path] =", out)
+
+      switch (this.options.type) {
+        case "rerun":
+          this.sendCmd("rerun", out)
+          break
+        case "save":
+          this.sendCmd("save", out)
+          break
+        default:
+          console.error("重新编译后,不进行任何操作")
+          break
+      }
+    })
+  }
+}
+
 const entry_file = path.resolve("./src/main.ts")
 const alias_path = path.resolve("./src/modules")
 const output_path = path.resolve("./dist")
